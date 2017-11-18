de36655930c623c8132ea3f579148e7776373cf2
Rostik Yuriev <rost4402255@gmail.com>
2017-11-18 15:11:47 +0200

    added argum

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
-function randArr() {
-  var arr = [1, "Rostik", 5, "str", 6];
-  var res = Math.floor(Math.random() * arr.length);
-  return arr[res];
+var arr = [1, "Rostik", 5, "str", 6];
+
+function randArr(any_arr) {
+  var res = Math.floor(Math.random() * any_arr.length);
+  return any_arr[res];
 }
 
-alert(randArr());
+alert(randArr(arr));
